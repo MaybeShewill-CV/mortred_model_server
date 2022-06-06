547ea77a8846d6a452396511c73426cdf2ea662d
luoyao <luoyao@baidu.com>
2022-06-06 15:14:00 +0800

    update base model

--- src/models/base_model.h
+++ src/models/base_model.h
@@ -8,6 +8,61 @@
 #ifndef MMAISERVER_BASE_MODEL_H
 #define MMAISERVER_BASE_MODEL_H
 
+#include "common/status_code.h"
+
+namespace morted {
+namespace models {
+
+template<class INPUT, class OUTPUT>
+class BaseAiModel {
+public:
+    /***
+    *
+    */
+    virtual ~BaseAiModel() = default;
+
+    /***
+     * 构造函数
+     * @param config
+     */
+    BaseAiModel() = default;
+
+    /***
+    * 赋值构造函数
+    * @param transformer
+    */
+    BaseAiModel(const BaseAiModel& BaseAiModel) = delete;
+
+    /***
+     * 复制构造函数
+     * @param transformer
+     * @return
+     */
+    BaseAiModel& operator=(const BaseAiModel& transformer) = delete;
+
+    /***
+     *
+     * @param cfg
+     * @return
+     */
+    virtual morted::common::StatusCode init(const decltype(toml::parse(""))& cfg) = 0;
+
+    /***
+     *
+     * @param input
+     * @param output
+     * @return
+     */
+    virtual morted::common::StatusCode run(const INPUT& input, OUTPUT& output) = 0;
+
+    /***
+     *
+     * @return
+     */
+    virtual bool is_successfully_initialized() const = 0;
+};
+}
+}
 
 
 #endif //MMAISERVER_BASE_MODEL_H
