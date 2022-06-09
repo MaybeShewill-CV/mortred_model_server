75f1d7d1b6770bb1da4210c28e60158100a65297
luoyao <luoyao@baidu.com>
2022-06-09 17:36:06 +0800

    add scene segmentation task

---
+++ src/factory/scene_segmentation_task.h
@@ -0,0 +1,42 @@
+/************************************************
+* Copyright MaybeShewill-CV. All Rights Reserved.
+* Author: MaybeShewill-CV
+* File: scene_segmentation.h
+* Date: 22-6-9
+************************************************/
+
+#ifndef MM_AI_SERVER_SCENE_SEGMENTATION_H
+#define MM_AI_SERVER_SCENE_SEGMENTATION_H
+
+#include "factory/base_factory.h"
+#include "factory/register_marco.h"
+#include "models/base_model.h"
+#include "models/scene_segmentation/bisenetv2.h"
+
+namespace morted {
+namespace factory {
+
+using morted::factory::ModelFactory;
+using morted::models::BaseAiModel;
+
+namespace scene_segmentation {
+using morted::models::scene_segmentation::BiseNetV2;
+
+/***
+ * create bisenetv2 scene segmentation instance
+ * @tparam INPUT
+ * @tparam OUTPUT
+ * @param detector_name
+ * @return
+ */
+template<typename INPUT, typename OUTPUT>
+static std::unique_ptr<BaseAiModel<INPUT, OUTPUT> > create_bisenetv2_segmentor(const std::string& segmentor_name) {
+    REGISTER_AI_MODEL(BiseNetV2, segmentor_name, INPUT, OUTPUT)
+    return ModelFactory<BaseAiModel<INPUT, OUTPUT> >::get_instance().get_model(segmentor_name);
+}
+
+}
+}
+}
+
+#endif //MM_AI_SERVER_SCENE_SEGMENTATION_H
