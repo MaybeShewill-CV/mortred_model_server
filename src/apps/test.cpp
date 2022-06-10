7b94907aeea7c28a37470c87f2d4f4453a5a1b95
MaybeShewill <howard327@163.com>
2022-06-11 01:05:56 +0800

    update

--- src/apps/test.cpp
+++ src/apps/test.cpp
@@ -32,37 +32,46 @@ void do_segmentation() {
 
 int main(int argc, char** argv) {
 
-    WFFacilities::WaitGroup wait_group(1);
-    auto* series = Workflow::create_series_work(
-            WFTaskFactory::create_empty_task(),
-            [&](const SeriesWork* work){
-                LOG(INFO) << "Series complete at: " << Timestamp::now().to_format_str();
-            });
-
-    WFTimerTask* get_image_data = WFTaskFactory::create_timer_task(
-            1000000 * 5, [&](const WFTimerTask* task) {
-                LOG(INFO) << "successfully get image data at: " << Timestamp::now().to_format_str();
-            });
-    WFCounterTask *counter = WFTaskFactory::create_counter_task("count", 1, [&](const WFCounterTask* task) {
-        LOG(INFO) << "counter task complete at: " << Timestamp::now().to_format_str();
-        auto* postprocess_work = WFTaskFactory::create_go_task("postprocess", [&]() {
-            LOG(INFO) << "do postprocess task at: " << Timestamp::now().to_format_str();
-        });
-        *series_of(task) << postprocess_work;
-    });
-    counter->start();
-
-    auto* segmentaiton_work = WFTaskFactory::create_go_task("do_segmentation", do_segmentation);
-    segmentaiton_work->set_callback([&](const WFGoTask* task) {
-        LOG(INFO) << "complete segmentation at:" << Timestamp::now().to_format_str();
-        WFTaskFactory::count_by_name("count");
+    WFHttpServer server([](WFHttpTask *task) {
+        task->get_resp()->append_output_body("<html>Hello World!</html>");
     });
 
-    *series << get_image_data;
-    *series << segmentaiton_work;
-    series->start();
+    if (server.start(80) == 0) { // start server on port 8888
+        getchar(); // press "Enter" to end.
+        server.stop();
+    }
 
-    LOG(INFO) << "start whole process at: " << Timestamp::now().to_format_str();
-    wait_group.wait();
+//    WFFacilities::WaitGroup wait_group(1);
+//    auto* series = Workflow::create_series_work(
+//            WFTaskFactory::create_empty_task(),
+//            [&](const SeriesWork* work){
+//                LOG(INFO) << "Series complete at: " << Timestamp::now().to_format_str();
+//            });
+//
+//    WFTimerTask* get_image_data = WFTaskFactory::create_timer_task(
+//            1000000 * 5, [&](const WFTimerTask* task) {
+//                LOG(INFO) << "successfully get image data at: " << Timestamp::now().to_format_str();
+//            });
+//    WFCounterTask *counter = WFTaskFactory::create_counter_task("count", 1, [&](const WFCounterTask* task) {
+//        LOG(INFO) << "counter task complete at: " << Timestamp::now().to_format_str();
+//        auto* postprocess_work = WFTaskFactory::create_go_task("postprocess", [&]() {
+//            LOG(INFO) << "do postprocess task at: " << Timestamp::now().to_format_str();
+//        });
+//        *series_of(task) << postprocess_work;
+//    });
+//    counter->start();
+//
+//    auto* segmentaiton_work = WFTaskFactory::create_go_task("do_segmentation", do_segmentation);
+//    segmentaiton_work->set_callback([&](const WFGoTask* task) {
+//        LOG(INFO) << "complete segmentation at:" << Timestamp::now().to_format_str();
+//        WFTaskFactory::count_by_name("count");
+//    });
+//
+//    *series << get_image_data;
+//    *series << segmentaiton_work;
+//    series->start();
+//
+//    LOG(INFO) << "start whole process at: " << Timestamp::now().to_format_str();
+//    wait_group.wait();
     return 1;
 }
