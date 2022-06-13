9b59605f0552433b80615c87084d03f9cf4e57e6
luoyao <luoyao@baidu.com>
2022-06-13 19:52:28 +0800

    add mobilenetv2

---
+++ src/models/classification/mobilenetv2.h
@@ -0,0 +1,84 @@
+/************************************************
+* Copyright MaybeShewill-CV. All Rights Reserved.
+* Author: MaybeShewill-CV
+* File: MobileNetv2.h
+* Date: 22-6-13
+************************************************/
+
+#ifndef MM_AI_SERVER_MOBILENETV2_H
+#define MM_AI_SERVER_MOBILENETV2_H
+
+#include <memory>
+
+#include "toml/toml.hpp"
+
+#include "models/base_model.h"
+#include "models/model_io_define.h"
+#include "common/status_code.h"
+
+namespace morted {
+namespace models {
+namespace classification {
+
+template<typename INPUT, typename OUTPUT>
+class MobileNetv2 : public morted::models::BaseAiModel<INPUT, OUTPUT> {
+public:
+
+    /***
+    * 构造函数
+    * @param config
+    */
+    MobileNetv2();
+
+    /***
+     *
+     */
+    ~MobileNetv2() override;
+
+    /***
+    * 赋值构造函数
+    * @param transformer
+    */
+    MobileNetv2(const MobileNetv2& transformer) = delete;
+
+    /***
+     * 复制构造函数
+     * @param transformer
+     * @return
+     */
+    MobileNetv2& operator=(const MobileNetv2& transformer) = delete;
+
+    /***
+     *
+     * @param toml
+     * @return
+     */
+    morted::common::StatusCode init(const decltype(toml::parse(""))& cfg) override;
+
+    /***
+     *
+     * @param input
+     * @param output
+     * @return
+     */
+    morted::common::StatusCode run(const INPUT& input, std::vector<OUTPUT>& output) override;
+
+
+    /***
+     * if db text detector successfully initialized
+     * @return
+     */
+    bool is_successfully_initialized() const override;
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> _m_pimpl;
+};
+
+}
+}
+}
+
+#include "mobilenetv2.inl"
+
+#endif //MM_AI_SERVER_MOBILENETV2_H
---
+++ src/models/classification/mobilenetv2.inl
@@ -0,0 +1,8 @@
+/************************************************
+* Copyright MaybeShewill-CV. All Rights Reserved.
+* Author: MaybeShewill-CV
+* File: MobileNetv2.cpp
+* Date: 22-6-13
+************************************************/
+
+#include "mobilenetv2.h"
