5b954c84a5e44946014ebc67b0efbdede3049ffc
MaybeShewill <howard327@163.com>
2022-07-02 02:08:43 +0800

    fix

--- scripts/server/test_server.py
+++ scripts/server/test_server.py
@@ -77,9 +77,11 @@ def locust_test_mode(url, src_image_path, u, r, t):
         r (_type_): _description_
         t (_type_): _description_
     """
-    import locust_performance
-    locust_performance.URL = url
-    locust_performance.SRC_IMAGE_PATH = src_image_path
+    content = open('locust_performance.py', 'r').readlines()
+    content[17] = 'URL = \'{:s}\''.format(url)
+    content[18] = 'SRC_IMAGE_PATH = \'{:s}\''.format(src_image_path)
+    open('locust_performance.py', 'w').writelines(content)
+    
     command = 'locust -f ./server/locust_performance.py --host={:s} --headless -u {:d} -r {:d} -t {:s}'.format(url, u, r, t)
     os.system(command=command)
 
