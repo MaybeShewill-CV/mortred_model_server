fb274c5a633e63036bf836aca8697be6911ca6a9
MaybeShewill <howard327@163.com>
2022-11-16 22:40:23 +0800

    update code

--- src/server/object_detection/libface_det_server.h
+++ src/server/object_detection/libface_det_server.h
@@ -19,7 +19,7 @@ class LibfaceDetServer : public jinq::server::BaseAiServer {
 public:
 
     /***
-    * 构造函数
+    * constructor
     * @param config
     */
     LibfaceDetServer();
@@ -30,13 +30,13 @@ public:
     ~LibfaceDetServer() override;
 
     /***
-    * 赋值构造函数
+    * constructor
     * @param transformer
     */
     LibfaceDetServer(const LibfaceDetServer& transformer) = delete;
 
     /***
-     * 复制构造函数
+     * constructor
      * @param transformer
      * @return
      */
