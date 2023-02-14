278eaff3b343acc7d3803ebc21379132773f09af
MaybeShewill <howard327@163.com>
2023-02-14 21:54:02 +0800

    update

--- src/apps/server/registration/test.cpp
+++ src/apps/server/registration/test.cpp
@@ -14,6 +14,8 @@ int main(int argc, char** argv) {
     std::map<std::string, std::string> conds;
     auto res = helper.select("", {"", ""}, conds);
 
+    LOG(INFO) << "select info length: " << res.size();
+
     for (auto& row : res) {
         for (auto& col : row) {
             LOG(INFO) << "key: " << col.first << ", value: " << std::get<std::string>(col.second);
