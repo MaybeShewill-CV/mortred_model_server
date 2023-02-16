7d5a62c39182c228230b799a4098390923debf52
MaybeShewill <howard327@163.com>
2023-02-16 16:04:14 +0800

    update

--- src/apps/server/registration/test.cpp
+++ src/apps/server/registration/test.cpp
@@ -12,13 +12,15 @@ using jinq::registration::mysql::MySqlHelper;
 int main(int argc, char** argv) {
 
     MySqlDBConfig db_cfg;
-    std::string cfg_path = "../conf/server/registration_center/registration_config.ini";
-    db_cfg.init(cfg_path);
+    db_cfg.set_db_name("mortred_ai_serer");
+    db_cfg.set_host("localhost");
+    db_cfg.set_user_name("root");
+    db_cfg.set_user_pw("327205");
 
     MySqlHelper helper;
     helper.init(db_cfg);
     std::map<std::string, std::string> conds;
-    auto res = helper.select("mmai_projects", {"id", "name"}, conds);
+    auto res = helper.select("mmai_service_instances", {"ip_address", "service_id"}, conds);
 
     LOG(INFO) << "select result: " << res;
     return 0;
