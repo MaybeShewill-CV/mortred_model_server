208e67ca074d74535805024e7a227417acd8747b
MaybeShewill <howard327@163.com>
2023-05-23 20:55:18 +0800

    update

--- src/common/status_code.cpp
+++ src/common/status_code.cpp
@@ -43,7 +43,8 @@ std::map<int, std::string>& get_error_code_table() {
         { StatusCode::MYSQL_UPDATE_FAILED, "exec update sql failed"},
         { StatusCode::MYSQL_DELETE_FAILED, "exec delete sql failed"},
 
-        { StatusCode::ROUTER_ADD_HANDLER_FAILED, "add handler to router table faile"},
+        { StatusCode::ROUTER_ADD_HANDLER_FAILED, "add handler to router table failed"},
+        { StatusCode::ROUTER_GET_HANDLER_FAILED, "get handler from router table failed"},
 
     };
     return m_error_code_table;
