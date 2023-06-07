006da282ed752dc62c033ad694774b892204a190
luoyao <luoyao@baidu.com>
2023-06-07 14:35:02 +0800

    update

--- src/models/segment_anything/sam_vit_encoder.h
+++ src/models/segment_anything/sam_vit_encoder.h
@@ -8,6 +8,74 @@
 #ifndef MORTRED_MODEL_SERVER_SAMVITENCODER_H
 #define MORTRED_MODEL_SERVER_SAMVITENCODER_H
 
-class SamVitEncoder {};
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+#include "toml/toml.hpp"
+
+#include "common/status_code.h"
+
+namespace jinq {
+namespace models {
+namespace segment_anything {
+
+/***
+ *
+ */
+class SamVitEncoder {
+  public:
+    /***
+    * constructor
+    * @param config
+     */
+    SamVitEncoder();
+
+    /***
+     *
+     */
+    ~SamVitEncoder();
+
+    /***
+    * constructor
+    * @param transformer
+     */
+    SamVitEncoder(const SamVitEncoder& transformer) = delete;
+
+    /***
+     * constructor
+     * @param transformer
+     * @return
+     */
+    SamVitEncoder& operator=(const SamVitEncoder& transformer) = delete;
+
+    /***
+     *
+     * @param toml
+     * @return
+     */
+    jinq::common::StatusCode init(const decltype(toml::parse(""))& cfg);
+
+    /***
+     *
+     * @param input_image
+     * @param image_embeddings
+     * @return
+     */
+    jinq::common::StatusCode get_embedding(const cv::Mat& input_image, std::vector<float>& image_embeddings);
+
+
+    /***
+     * if db text detector successfully initialized
+     * @return
+     */
+    bool is_successfully_initialized() const;
+
+  private:
+    class Impl;
+    std::unique_ptr<Impl> _m_pimpl;
+};
+}
+}
+}
 
 #endif // MORTRED_MODEL_SERVER_SAMVITENCODER_H
