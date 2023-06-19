fd721bd009773a3e7dcee80a0b1a6d33000fc383
luoyao <luoyao@baidu.com>
2023-06-19 14:13:49 +0800

    update

--- src/registration/registration_server.cpp
+++
@@ -1,9 +0,0 @@
-/************************************************
- * Copyright MaybeShewill-CV. All Rights Reserved.
- * Author: MaybeShewill-CV
- * File: RegistrationHelper.cpp
- * Date: 23-2-21
- ************************************************/
-
-#include "registration_server.h"
-
