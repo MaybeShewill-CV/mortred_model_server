c5d5197bcd86705bc24a4a53f8df6a5220438969
luoyao <luoyao@baidu.com>
2023-07-03 17:11:59 +0800

    update

--- src/apps/model_benchmark/sam/fast_sam_benchmark.cpp
+++ src/apps/model_benchmark/sam/fast_sam_benchmark.cpp
@@ -14,9 +14,11 @@
 #include "opencv2/opencv.hpp"
 
 #include "common/cv_utils.h"
+#include "common/file_path_util.h"
 #include "models/segment_anything/fast_sam_segmentor.h"
 
 using jinq::common::CvUtils;
+using jinq::common::FilePathUtil;
 using jinq::models::segment_anything::FastSamSegmentor;
 
 int main(int argc, char** argv) {
@@ -38,24 +40,26 @@ int main(int argc, char** argv) {
     }
     cv::Mat input_image = cv::imread(input_image_path, cv::IMREAD_UNCHANGED);
 
-    std::vector<cv::Mat> masks;
     LOG(INFO) << "Start benchmarking sam predict interface ...";
+    cv::Mat everything_mask;
     for (auto i = 0; i < 1; ++i) {
         auto t_start = std::chrono::system_clock::now();
-        fast_sam_model.predict(
-            input_image,
-            {
-                cv::Rect(483, 683, 158, 132),
-                cv::Rect(220, 327, 430, 122),
-                cv::Rect(77, 78, 58, 176),
-                cv::Rect(972, 464, 111, 52)
-            },
-            masks);
+        fast_sam_model.everything(input_image, everything_mask);
         auto t_end = std::chrono::system_clock::now();
         auto t_cost = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count();
         LOG(INFO) << "... infer: " << i << ", cost time: " << t_cost;
     }
+    cv::Mat everything_color_mask;
+    CvUtils::colorize_sam_everything_mask(everything_mask, everything_color_mask);
+    cv::Mat merge_result;
+    cv::addWeighted(input_image, 0.65, everything_color_mask, 0.35, 0.0, merge_result);
 
+    std::string output_file_name = FilePathUtil::get_file_name(input_image_path);
+    output_file_name = output_file_name.substr(0, output_file_name.find_last_of('.')) + "_fastsam_everything_result.png";
+    std::string output_path = FilePathUtil::concat_path(
+        "../demo_data/model_test_input/sam", output_file_name);
+    cv::imwrite(output_path, merge_result);
+    LOG(INFO) << "fast-sam everything result image has been written into: " << output_path;
 
-    LOG(INFO) << "complete";
+    return 1;
 }
