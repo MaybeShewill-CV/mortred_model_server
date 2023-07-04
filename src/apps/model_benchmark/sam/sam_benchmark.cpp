c18ce676af9d8af58449bec559da86e51ddbe6d4
luoyao <luoyao@baidu.com>
2023-07-04 17:10:55 +0800

    update

--- src/apps/model_benchmark/sam/sam_benchmark.cpp
+++ src/apps/model_benchmark/sam/sam_benchmark.cpp
@@ -82,5 +82,5 @@ int main(int argc, char** argv) {
     cv::Mat color_output;
     CvUtils::visualize_sam_output_masks(input_image, masks, color_output);
     cv::imwrite(output_path, color_output);
-    LOG(INFO) << "fast-sam everything result image has been written into: " << output_path;
+    LOG(INFO) << "sam everything result image has been written into: " << output_path;
 }
