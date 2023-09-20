cdb7a5810ad484999fe45d2c9566a59b4164cb3b
luoyao <luoyao@baidu.com>
2023-09-20 11:41:43 +0800

    update

--- src/models/segment_anything/sam_trt_everything_decoder.h
+++ src/models/segment_anything/sam_trt_everything_decoder.h
@@ -1,13 +1,96 @@
 /************************************************
  * Copyright MaybeShewill-CV. All Rights Reserved.
  * Author: MaybeShewill-CV
- * File: SamTrtEverythingDecoder.h
+ * File: sam_trt_everything_decoder.h
  * Date: 23-9-20
  ************************************************/
 
-#ifndef MORTRED_MODEL_SERVER_SAMTRTEVERYTHINGDECODER_H
-#define MORTRED_MODEL_SERVER_SAMTRTEVERYTHINGDECODER_H
+#ifndef MORTRED_MODEL_SERVER_SAM_TRT_EVERYTHING_DECODER_H
+#define MORTRED_MODEL_SERVER_SAM_TRT_EVERYTHING_DECODER_H
 
-class SamTrtEverythingDecoder {};
+#include <vector>
 
-#endif // MORTRED_MODEL_SERVER_SAMTRTEVERYTHINGDECODER_H
+#include <opencv2/opencv.hpp>
+#include "toml/toml.hpp"
+
+#include "common/status_code.h"
+
+namespace jinq {
+namespace models {
+namespace segment_anything {
+
+/***
+ *
+ */
+class SamTrtEverythingDecoder {
+  public:
+    /***
+    * constructor
+    * @param config
+     */
+    SamTrtEverythingDecoder();
+
+    /***
+     *
+     */
+    ~SamTrtEverythingDecoder();
+
+    /***
+    * constructor
+    * @param transformer
+     */
+    SamTrtEverythingDecoder(const SamTrtEverythingDecoder& transformer) = delete;
+
+    /***
+     * constructor
+     * @param transformer
+     * @return
+     */
+    SamTrtEverythingDecoder& operator=(const SamTrtEverythingDecoder& transformer) = delete;
+
+    /***
+     *
+     * @param toml
+     * @return
+     */
+    jinq::common::StatusCode init(const decltype(toml::parse(""))& cfg);
+
+    /***
+     *
+     * @param ori_img_size
+     */
+    void set_ori_image_size(const cv::Size& ori_img_size);
+
+    /***
+     *
+     * @param ori_img_size
+     */
+    void set_encoder_input_size(const cv::Size& input_node_size);
+
+    /***
+     *
+     * @param image_embeddings
+     * @param bboxes
+     * @param predicted_masks
+     * @return
+     */
+    jinq::common::StatusCode decode(
+        const std::vector<float>& image_embeddings,
+        const std::vector<std::vector<cv::Point2f> >& points,
+        std::vector<cv::Mat>& predicted_masks);
+
+    /***
+     * if model successfully initialized
+     * @return
+     */
+    bool is_successfully_initialized() const;
+
+  private:
+    class Impl;
+    std::unique_ptr<Impl> _m_pimpl;
+};
+}
+}
+}
+
+#endif // MORTRED_MODEL_SERVER_SAM_TRT_EVERYTHING_DECODER_H
