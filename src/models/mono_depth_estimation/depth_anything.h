3e59f7bf98594c9514419674658076d1a1e01980
luoyao <luoyao@baidu.com>
2024-01-25 17:47:57 +0800

    update metric3d conf

---
+++ src/models/mono_depth_estimation/depth_anything.h
@@ -0,0 +1,13 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: DepthAnything.h
+ * Date: 24-1-25
+ ************************************************/
+
+#ifndef MORTRED_MODEL_SERVER_DEPTHANYTHING_H
+#define MORTRED_MODEL_SERVER_DEPTHANYTHING_H
+
+class DepthAnything {};
+
+#endif // MORTRED_MODEL_SERVER_DEPTHANYTHING_H
---
+++ src/models/mono_depth_estimation/depth_anything.inl
@@ -0,0 +1,8 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: DepthAnything.cpp
+ * Date: 24-1-25
+ ************************************************/
+
+#include "DepthAnything.h"
