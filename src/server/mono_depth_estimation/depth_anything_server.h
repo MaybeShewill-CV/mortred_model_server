9d3a34d69e88ddc34dc9c47f13b15a6592528c90
luoyao <luoyao@baidu.com>
2024-01-26 11:40:35 +0800

    add depth anything server

--- src/server/mono_depth_estimation/depth_anything_server.h
+++ src/server/mono_depth_estimation/depth_anything_server.h
@@ -5,9 +5,68 @@
  * Date: 24-1-26
  ************************************************/
 
-#ifndef MORTRED_MODEL_SERVER_DEPTHANYTHINGSERVER_H
-#define MORTRED_MODEL_SERVER_DEPTHANYTHINGSERVER_H
+#ifndef MORTRED_MODEL_SERVER_DEPTH_ANYTHING_SERVER_H
+#define MORTRED_MODEL_SERVER_DEPTH_ANYTHING_SERVER_H
 
-class DepthAnythingServer {};
+#include <memory>
 
-#endif // MORTRED_MODEL_SERVER_DEPTHANYTHINGSERVER_H
+#include "server/abstract_server.h"
+
+namespace jinq {
+namespace server {
+namespace mono_depth_estimation {
+class DepthAnythingServer : public jinq::server::BaseAiServer {
+  public:
+    
+    /***
+    * constructor
+    * @param config
+     */
+    DepthAnythingServer();
+
+    /***
+     *
+     */
+    ~DepthAnythingServer() override;
+
+    /***
+    * constructor
+    * @param transformer
+     */
+    DepthAnythingServer(const DepthAnythingServer& transformer) = delete;
+
+    /***
+     * constructor
+     * @param transformer
+     * @return
+     */
+    DepthAnythingServer& operator=(const DepthAnythingServer& transformer) = delete;
+
+    /***
+     *
+     * @param toml
+     * @return
+     */
+    jinq::common::StatusCode init(const decltype(toml::parse(""))& cfg) override;
+
+    /***
+     *
+     * @param task
+     */
+    void serve_process(WFHttpTask* task) override;
+
+    /***
+     *
+     * @return
+     */
+    bool is_successfully_initialized() const override;
+
+  private:
+    class Impl;
+    std::unique_ptr<Impl> _m_impl;
+};
+}
+}
+}
+
+#endif // MORTRED_MODEL_SERVER_DEPTH_ANYTHING_SERVER_H
