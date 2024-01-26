0dde9e6af20ce1032622379d21e160b0e123b75f
luoyao <luoyao@baidu.com>
2024-01-26 11:40:13 +0800

    add depth anything server cfg

---
+++ src/server/mono_depth_estimation/depth_anything_server.cpp
@@ -0,0 +1,8 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: DepthAnythingServer.cpp
+ * Date: 24-1-26
+ ************************************************/
+
+#include "DepthAnythingServer.h"
---
+++ src/server/mono_depth_estimation/depth_anything_server.h
@@ -0,0 +1,13 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: DepthAnythingServer.h
+ * Date: 24-1-26
+ ************************************************/
+
+#ifndef MORTRED_MODEL_SERVER_DEPTHANYTHINGSERVER_H
+#define MORTRED_MODEL_SERVER_DEPTHANYTHINGSERVER_H
+
+class DepthAnythingServer {};
+
+#endif // MORTRED_MODEL_SERVER_DEPTHANYTHINGSERVER_H
