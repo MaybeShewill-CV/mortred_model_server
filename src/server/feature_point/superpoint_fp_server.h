3746ea90befb606b5963411ae3076a2356d3895e
luoyao <luoyao@baidu.com>
2024-03-22 11:34:58 +0800

    update

--- src/server/feature_point/superpoint_fp_server.h
+++ src/server/feature_point/superpoint_fp_server.h
@@ -5,8 +5,8 @@
 * Date: 22-6-29
 ************************************************/
 
-#ifndef MM_AI_SERVER_SUPERPOINTFPSERVER_H
-#define MM_AI_SERVER_SUPERPOINTFPSERVER_H
+#ifndef MORTRED_MODEL_SERVER_SUPERPOINT_FP_SERVER_H
+#define MORTRED_MODEL_SERVER_SUPERPOINT_FP_SERVER_H
 
 #include <memory>
 
@@ -69,4 +69,4 @@ private:
 }
 }
 
-#endif //MM_AI_SERVER_SUPERPOINTFPSERVER_H
+#endif //MORTRED_MODEL_SERVER_SUPERPOINT_FP_SERVER_H
--- src/server/object_detection/libface_det_server.h
+++ src/server/object_detection/libface_det_server.h
@@ -5,8 +5,8 @@
 * Date: 22-6-26
 ************************************************/
 
-#ifndef MM_AI_SERVER_LIBFACEDETSERVER_H
-#define MM_AI_SERVER_LIBFACEDETSERVER_H
+#ifndef MORTRED_MODEL_SERVER_LIBFACE_DET_SERVER_H
+#define MORTRED_MODEL_SERVER_LIBFACE_DET_SERVER_H
 
 #include <memory>
 
@@ -69,4 +69,4 @@ private:
 }
 }
 
-#endif //MM_AI_SERVER_LIBFACEDETSERVER_H
+#endif //MORTRED_MODEL_SERVER_LIBFACE_DET_SERVER_H
