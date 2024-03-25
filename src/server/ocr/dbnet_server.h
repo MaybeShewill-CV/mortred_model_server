3c28018b4db7ac5d360c232dbdb6ccba02c4a81b
luoyao <luoyao@baidu.com>
2024-03-25 11:57:21 +0800

    update

--- src/server/ocr/dbnet_server.h
+++ src/server/ocr/dbnet_server.h
@@ -5,8 +5,8 @@
 * Date: 22-7-04
 ************************************************/
 
-#ifndef MM_AI_SERVER_DBNETSERVER_H
-#define MM_AI_SERVER_DBNETSERVER_H
+#ifndef MORTRED_MODEL_SERVER_DBNET_SERVER_H
+#define MORTRED_MODEL_SERVER_DBNET_SERVER_H
 
 #include <memory>
 
@@ -69,4 +69,4 @@ private:
 }
 }
 
-#endif //MM_AI_SERVER_DBNETSERVER_H
+#endif //MORTRED_MODEL_SERVER_DBNET_SERVER_H
