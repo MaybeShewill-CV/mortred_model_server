2f49792a41842323ddc5ef801bb6a6d6d17ba0d5
luoyao <luoyao@baidu.com>
2024-04-24 19:53:20 +0800

    update

---
+++ src/apps/model_benchmark/diffusion/ddpm_sampler_benchmark.cpp
@@ -0,0 +1,6 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: ddpm_sampler_benchmark.cpp
+ * Date: 24-4-24
+ ************************************************/
