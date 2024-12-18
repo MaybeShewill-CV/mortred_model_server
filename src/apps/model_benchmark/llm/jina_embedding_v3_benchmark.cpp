c7a4da510f74a62813aecac05033eb837badbe1e
luoyao <luoyao@baidu.com>
2024-12-18 19:26:17 +0800

    update

---
+++ src/apps/model_benchmark/llm/jina_embedding_v3_benchmark.cpp
@@ -0,0 +1,6 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: jina_embedding_v3_benchmark.cpp
+ * Date: 24-12-18
+ ************************************************/
