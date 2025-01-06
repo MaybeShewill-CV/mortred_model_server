c1bcf7cb8d581d8c691c6df983d43105e47c3edf
luoyao <luoyao@baidu.com>
2025-01-06 16:07:17 +0800

    update stb_image

---
+++ src/models/llm/qwen2_vl/qwen2_vl.h
@@ -0,0 +1,147 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: qwen2_vl.h
+ * Date: 25-1-6
+ ************************************************/
+
+#ifndef MORTRED_MODEL_SERVER_QWEN2_VL_H
+#define MORTRED_MODEL_SERVER_QWEN2_VL_H
+
+#include <memory>
+
+#include "toml/toml.hpp"
+#include "llama_cpp/llama.h"
+
+#include "common/status_code.h"
+#include "models/base_model.h"
+#include "models/model_io_define.h"
+#include "models/llm/llm_datatype.hpp"
+
+namespace jinq {
+namespace models {
+namespace llm {
+
+namespace qwen2_vl {
+
+template <typename INPUT, typename OUTPUT>
+class Qwen2VL : public jinq::models::BaseAiModel<INPUT, OUTPUT> {
+  public:
+    /***
+    * constructor
+    * @param config
+     */
+    Qwen2VL();
+
+    /***
+     *
+     */
+    ~Qwen2VL() override;
+
+    /***
+    * constructor
+    * @param transformer
+     */
+    Qwen2VL(const Qwen2VL &transformer) = delete;
+
+    /***
+     * constructor
+     * @param transformer
+     * @return
+     */
+    Qwen2VL &operator=(const Qwen2VL &transformer) = delete;
+
+    /***
+     *
+     * @param toml
+     * @return
+     */
+    jinq::common::StatusCode init(const toml::value& cfg) override;
+
+    /***
+     *
+     * @param input
+     * @param output
+     * @return
+     */
+    jinq::common::StatusCode run(const INPUT &input, OUTPUT &output) override;
+
+    /***
+     *
+     * @param prompt
+     * @param prompt_tokens
+     * @param add_special
+     * @return
+     */
+    jinq::common::StatusCode tokenize(const std::string &prompt, std::vector<llama_token> &prompt_tokens, bool add_special = true);
+
+    /***
+     *
+     * @param prompt
+     * @param out_embeddings
+     * @param pool_type
+     * @param truncated
+     * @param max_seq_len
+     * @param do_norm
+     * @return
+     */
+    jinq::common::StatusCode get_embedding(
+        const std::string& prompt, std::vector<std::vector<float> >& out_embeddings, const std::string& pool_type = "mean",
+        bool truncated=true, int32_t max_seq_len=512, bool do_norm=true);
+
+    /***
+     *
+     * @param input
+     * @param output
+     * @return
+     */
+    jinq::common::StatusCode text_completion(const std::string& prompt, std::string& generate_output);
+
+    /***
+     *
+     * @param dialog
+     * @param generate_output
+     * @param truncate
+     * @return
+     */
+    jinq::common::StatusCode chat_completion(Dialog& dialog, std::string& generate_output);
+
+    /***
+     *
+     * @param dialog
+     * @param add_ass
+     * @param out_formatted_str
+     * @return
+     */
+    jinq::common::StatusCode apply_chat_template(const Dialog& dialog, bool add_ass, std::string& out_formatted_str);
+
+    /***
+     *
+     * @return
+     */
+    jinq::models::llm::ModelStatus get_model_stat() const;
+
+    /***
+     *
+     */
+    void clear_kv_cache_cell() const;
+
+    /***
+     * if model successfully initialized
+     * @return
+     */
+    bool is_successfully_initialized() const override;
+
+  private:
+    class Impl;
+    std::unique_ptr<Impl> _m_pimpl;
+};
+
+}
+}
+}
+}
+
+#include "qwen2_vl.inl"
+
+#endif // MORTRED_MODEL_SERVER_QWEN2_VL_H
---
+++ src/models/llm/qwen2_vl/qwen2_vl.inl
@@ -0,0 +1,8 @@
+/************************************************
+ * Copyright MaybeShewill-CV. All Rights Reserved.
+ * Author: MaybeShewill-CV
+ * File: Qwen2VL.cpp
+ * Date: 25-1-6
+ ************************************************/
+
+#include "qwen2_vl.h"
